8a7ea3bba27d8a21ad880fa324bb9f0075d10c5b
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-04-17 20:50:14 -0400

    fixies

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,33 @@
 #!/usr/bin/node
-const request = require("request");
-if (process.argv.length != 2) {
-  return;
+const request = require('request');
+if (process.argv.length !== 2) {
+  process.exit(1);
 }
 let movieId = process.argv[1];
 movieId = parseInt(movieId, 10);
 if (isNaN(movieId)) {
-  return;
+  process.exit(1);
 }
-let names = [];
+const names = [];
 request(
   `https://swapi-api.alx-tools.com/api/films/${movieId}`,
   function (error, response, body) {
+    if (error) {
+      return;
+    }
     const movieData = JSON.parse(body);
     const chars = movieData.characters;
     for (const charUrl in chars) {
       request(charUrl, function (error, response, body) {
+        if (error) {
+          return;
+        }
         const charData = JSON.parse(body);
         names.push(charData.name);
       });
     }
   }
 );
-for (name in names) {
+for (const name in names) {
   console.log(name);
 }
