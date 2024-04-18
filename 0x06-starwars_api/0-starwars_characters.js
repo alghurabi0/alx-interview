39a40654a335120486a745683c01aea9ca903c73
alghurabi0 <126355218+alghurabi0@users.noreply.github.com>
2024-04-18 02:53:54 +0200

    logs

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require("request");
 if (process.argv.length !== 2) {
+  console.log("length args error", process.argv);
   process.exit(1);
 }
 let movieId = process.argv[1];
 movieId = parseInt(movieId, 10);
 if (isNaN(movieId)) {
+  console.log("int str argv error", movieId);
   process.exit(1);
 }
 const names = [];
@@ -13,21 +15,30 @@ request(
   `https://swapi-api.alx-tools.com/api/films/${movieId}`,
   function (error, response, body) {
     if (error) {
+      console.log("movie req error", error);
       return;
     }
+    console.log("body", body);
+    console.log("response", response);
     const movieData = JSON.parse(body);
+    console.log("movieData", movieData);
     const chars = movieData.characters;
     for (const charUrl in chars) {
       request(charUrl, function (error, response, body) {
         if (error) {
+          console.log("char error", error);
           return;
         }
+        console.log("body", body);
+        console.log("response", response);
         const charData = JSON.parse(body);
+        console.log("charData", charData);
         names.push(charData.name);
       });
     }
   }
 );
+console.log(names);
 for (const name in names) {
   console.log(name);
 }
